8bde9239ef52e4f6cbb91e82789f77be27b63c2a
Jan Rothen <jan.rothen@me.com>
2017-09-10 00:34:19 +0200

    Almost but not really

--- src/blockchainjob.py
+++ src/blockchainjob.py
@@ -27,14 +27,28 @@ def should_store_hash_in_blockchain(file):
 	return True
 
 def store_hash_in_blockchain(hash, web3):
-	print 'Storing hash: {}\n'.format(hash)
+	print 'Storing hash: {}'.format(hash)
 	print 'Current block height: {}'.format(web3.eth.blockNumber)
 	
-	address_from = '0x4f7696940Cfe0C75c830da07435e65e5ebb610B0'
+	address_from = '0x4f7696940Cfe0C75c830da07435e65e5ebb610B0' # account
 	address_to = '0x85043213AFbA0eccd37F29DE0BB760b42EBd5d58'
 	private_key = '0c6ae2768077764b1c1ed3e6b52a2df64f01fd465a450b0a1a989ffbbbaecaac'
+	data = ' '.join(format(x, 'b') for x in bytearray(hash))
 	
-	
+	tx = Transaction(
+	nonce=web3.eth.getTransactionCount(address_from),
+	gasprice=42000, #web3.eth.gasPrice
+	startgas=30000,
+	to='0x85043213AFbA0eccd37F29DE0BB760b42EBd5d58',
+	value=0.1,
+	data=b' ',
+	)
+	tx.sign(private_key)
+	raw_tx = rlp.encode(tx)
+	raw_tx_hex = web3.toHex(raw_tx)
+	result = web3.eth.sendRawTransaction(raw_tx_hex)
+	print result
+
 
 def get_hash(file):
 	data = open(file, "rb")
